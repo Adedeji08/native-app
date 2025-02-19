5b2e5c2877575fc67378327fbc0920dffac09644
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2025-02-19 16:00:35 +0100

    flatlist

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {
+  FlatList,
   RefreshControl,
   ScrollView,
   StyleSheet,
@@ -9,45 +10,84 @@ import {
 
 export default function App() {
   const [items, setItems] = useState([
-    { key: 1, item: "Item 1" },
-    { key: 2, item: "Item 2" },
-    { key: 3, item: "Item 3" },
-    { key: 4, item: "Item 4" },
-    { key: 5, item: "Item 5" },
-    { key: 6, item: "Item 6" },
-    { key: 7, item: "Item 7" },
-    { key: 8, item: "Item 8" },
-    { key: 9, item: "Item 9" },
-    { key: 33, item: "Item 10" },
-    { key: 44, item: "Item 19" },
-    { key: 68, item: "Item 35" },
-    { key: 0, item: "Item 120" },
+    { name: "Item 1" },
+    { name: "Item 2" },
+    { name: "Item 3" },
+    { name: "Item 4" },
+    { name: "Item 5" },
+    { name: "Item 6" },
+    { name: "Item 7" },
+    { name: "Item 8" },
+    { name: "Item 9" },
+    { name: "Item 10" },
+    { name: "Item 19" },
+    { name: "Item 35" },
+    { name: "Item 120" },
   ]);
+
+  // const DATA = [
+  //    {
+  //     title: 'Title 1',
+  //     data: ['Item 1-1', 'Item 1-2', 'Item 1-3']
+  //    },
+  //    {
+  //     title: 'Title 2',
+  //     data: ['Item 2-1', 'Item 2-2', 'Item 2-3']
+  //    },
+  //    {
+  //     title: 'Title 3',
+  //     data: ['Item 3-1', 'Item 3-2', 'Item 3-3']
+  //    },
+  //    {
+  //     title: 'Title 4',
+  //     data: ['Item 4-1', 'Item 4-2', 'Item 4-3']
+  //    },
+  //    {
+  //     title: 'Title 5',
+  //     data: ['Item 5-1', 'Item 5-2', 'Item 5-3']
+  //    },
+  // ]
   const [Refreshing, setRefreshing] = useState(false);
   function onRefresh() {
     setRefreshing(true);
-    setItems([...items, { key: 69, item: "Item 69" }]);
+    setItems([...items, {name: "Item 69" }]);
     setRefreshing(false);
   }
   return (
-    <ScrollView
-      style={styles.body}
+    <FlatList
+      keyExtractor={(item, index) => index.toString()}
+      data={items}
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item?.name}</Text>
+        </View>
+      )}
       refreshControl={
-        <RefreshControl
-          refreshing={Refreshing}
-          onRefresh={onRefresh}
-          colors={["#ff00ff"]}
-        />
+             <RefreshControl
+               refreshing={Refreshing}
+               onRefresh={onRefresh}
+               colors={["#ff00ff"]}
+             />
       }
-    >
-      {items.map((object) => {
-        return (
-          <View style={styles.item} key={object?.key}>
-            <Text style={styles.text}>{object?.item}</Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+    />
+    // <ScrollView
+    //   style={styles.body}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={Refreshing}
+    //       onRefresh={onRefresh}
+    //       colors={["#ff00ff"]}
+    //     />
+    //   }
+    // >
+    //   {items.map((object) => {
+    //     return (
+    //       <View style={styles.item} key={object?.key}>
+    //         <Text style={styles.text}>{object?.item}</Text>
+    //       </View>
+    //     );
+    //   })}
+    // </ScrollView>
   );
 }
 
