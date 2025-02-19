7b9e5e5911583972fabb00886397f0db46a1a7c5
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2025-02-19 12:13:12 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Style Test');
+ 
+  function onClickHandler () {
+    setName('Style Test is Done')
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.body}>
+      <Text style={styles.text}>{name}</Text>
+      <Button title='Update State' onPress={onClickHandler}> </Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  body: {
+    width: '100%',
+    height: '50%', 
+    backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: '#ff00ff',
+    borderRadius: 10,
   },
+  text:{
+    color: "#000",
+    fontSize: 40,
+    fontStyle: 'italic',
+    margin: 10,
+  }
 });
