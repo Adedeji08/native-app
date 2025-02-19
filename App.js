27dfd101fda2521febbfd5790148ef2aae6b6e72
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2025-02-19 13:32:11 +0100

    flex

--- App.js
+++ App.js
@@ -1,37 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState('Style Test');
- 
-  function onClickHandler () {
-    setName('Style Test is Done')
-  }
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>{name}</Text>
-      <Button title='Update State' onPress={onClickHandler}> </Button>
-      <StatusBar style="auto" />
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
+      </View>
+      <View style={styles.view4}>
+        <Text style={styles.text}>4</Text>
+      </View>
+      <View style={styles.view5}>
+        <Text style={styles.text}>5</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
-    width: '100%',
-    height: '50%', 
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 10,
-    borderColor: '#ff00ff',
-    borderRadius: 10,
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#0000ff",
+    alignItems: "stretch",
+    justifyContent: "center",
   },
-  text:{
+  view1: {
+    flex:1,
+    backgroundColor: "#00ffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  view2: {
+     flex:1,
+    backgroundColor: "#f0f0ff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  view3: {
+     flex:1,
+    backgroundColor: "#ff00ff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  view4: {
+     width: 100,
+    height: 100,
+    backgroundColor: "#ff0f0f",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  view5: {
+     width: 100,
+    height: 100,
+    backgroundColor: "#ffff00",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
     color: "#000",
     fontSize: 40,
-    fontStyle: 'italic',
+    fontStyle: "italic",
     margin: 10,
-  }
+  },
 });
